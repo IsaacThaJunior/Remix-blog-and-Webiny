1eb40811f31d9ec190ce7c925b90b447aad1c227
IsaacThaJunior <isaacjunez669@gmail.com>
2022-07-31 19:17:29 +0100

    made everything look a little better

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,10 +2,10 @@ import { Link } from '@remix-run/react';
 
 export default function Index() {
 	return (
-		<div className='my-60 ml-60  flex'>
-			<h1 className="text-3xl text-center mr-80 font-bold ">Welcome to my Remix App</h1>
+		<div className='my-40 ml-80  '>
+			<h1 className="text-3xl mb-5 font-bold ">Welcome to my Remix App</h1>
 			<Link to='/posts'>
-				<button className='cursor-pointer inline-flex items-center py-2 px-3 text-sm ml-50 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"'>Go to blog</button>
+				<button className='cursor-pointer  inline-flex items-center py-2 px-3 text-sm ml-50 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"'>Go to blog</button>
 			</Link>
 		</div>
 	);
