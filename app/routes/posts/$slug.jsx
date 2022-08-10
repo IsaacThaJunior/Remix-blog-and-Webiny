cef029ef093d1e642ce2b1159eef76cef85e5abc
IsaacThaJunior <isaacjunez669@gmail.com>
2022-08-10 16:07:49 +0100

    clean up

--- app/routes/posts/$slug.jsx
+++ app/routes/posts/$slug.jsx
@@ -9,9 +9,8 @@ export const loader = async () => {
 
 	const graphqlQuery = {
 		query: `
-  query getPerPost($PostListWhereInput: PostListWhereInput) {
-    listPosts(where: $PostListWhereInput) {
-
+  query getPerPost {
+    listPosts {
       data  {
         title
           slug
@@ -52,10 +51,9 @@ export default function Slug() {
 		<div className="">
 			<div className="container mx-auto ml-50 mb-8 space-y-6 py-3">
 				<Link
-					to='/posts'
+					to="/posts"
 					className="cursor-pointer inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 				>
-					
 					<svg
 						className=" mr-2 w-4 h-4"
 						fill="currentColor"
@@ -63,9 +61,9 @@ export default function Slug() {
 						xmlns="http://www.w3.org/2000/svg"
 					>
 						<path
-							fill-rule="evenodd"
+							fillRule="evenodd"
 							d="M15.41 16.09L10.83 11.5L15.41 6.91L14 5.5L8 11.5L14 17.5L15.41 16.09Z"
-							clip-rule="evenodd"
+							clipRule="evenodd"
 						></path>
 					</svg>
 					BACK
